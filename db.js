200c0ec9fafa3de82d62a7a7dce3ae43bd686239
Khadhiri Abdul Subhan <ams.khadhiri@gmail.com>
2023-01-09 17:24:08 +0530

    Add query to get particular query and increment the nooftimesViewd

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
-const pool= new Pool({
-    user:"postgres",
-    password:"admin",
-    host:"localhost",
-    port:5432,
-    database:"StockDatabase"
+const pool = new Pool({
+  user: "postgres",
+  password: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "StockDatabase",
 });
 
-module.exports= pool;
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 5000
-const cors= require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5000;
+const cors = require("cors");
 const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/stockDetails',async(req,res)=>{
+app.get("/getSearchSuggestions", async (req, res) => {
+  try {
+    const allStockDetails = await pool.query("SELECT stockid,companyname,nselistedname FROM Stocks");
+    console.log(req.body);
+    res.json(allStockDetails.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-    try{
-        const allStockDetails = await pool.query("SELECT * FROM Stocks")
-        console.log(req.body);
-        res.json(allStockDetails.rows);
 
+// Get Particular stock information using stockId and increment the no of times viewed
+
+app.get('/stock/:stockId',async (req,res)=>{
+    try {
+        const {stockId} =req.params;
+        const updateNoOfTimesViewed= await pool.query("Update Stocks SET nooftimesviewed = COALESCE(nooftimesviewed,0) + $1 where stockid = $2",[1,stockId]);
+        const selectedStock = await pool.query("select * from Stocks where stockId = $1",[stockId]);
+
+        res.json(selectedStock?.rows?.[0]);
     }
     catch (err) {
         console.error(err.message);
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  });
- 
+  console.log(`Example app listening on port ${port}`);
+});
